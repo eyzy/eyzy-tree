e5bbe0f2ccc903b3afd7392830309a478fb8718d
amsik <phlyze@gmail.com>
2019-04-18 12:45:56 +0300

    Add tests

--- tests/utils/find.test.ts
+++ tests/utils/find.test.ts
@@ -3,7 +3,7 @@ import { find } from '../../src/utils/find'
 import { walkBreadth } from '../../src/utils/traveler'
 
 const collection = [
-  { text: 'Item 1' },
+  { text: 'Item 1', id: 'sg23-d3hs' },
   { text: 'Item 2', someProperty: false },
   { text: 'Item 3', someProperty: 'AWESOMEProperty' }
 ]
@@ -19,8 +19,19 @@ describe('Find', () => {
     expect(callFind({ lalal: /Item 33/ })).toBeNull()
   })
 
-  it('RegExp', () => {
+  it('Search by text', () => {
     expect(callFind(/^Item/)).toBe(collection[0])
+    expect(callFind('Item 1')).toBe(collection[0])
+  })
+
+  it('Search by id', () => {
+    expect(callFind('sg23-d3hs')).toBe(collection[0])
+
+    // TODO: should it works?
+    // expect(callFind(/sg23/)).toBe(collection[0])
+  })
+
+  it('RegExp', () => {
     expect(callFind({ text: /2$/ })).toBe(collection[1])
     expect(callFind({ someProperty: /awesome/ })).toBeNull()
     expect(callFind({ someProperty: /awesome/i })).toBe(collection[2])
